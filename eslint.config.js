da7f3632649b02c5a0069380de9aecd885441d3d
Penko Atanasov <atanasovpenko@gmail.com>
2025-03-30 21:02:17 +0300

    Adding eslint sort import config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default defineConfig([
     { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -11,7 +12,10 @@ export default defineConfig([
     tseslint.configs.recommended,
     {
         files: ["**/*.{jsx,tsx}"],
-        plugins: { react: pluginReact },
+        plugins: {
+            react: pluginReact,
+            "simple-import-sort": simpleImportSort,
+        },
         settings: {
             react: {
                 version: "detect",
@@ -30,6 +34,26 @@ export default defineConfig([
                 },
             ],
             "@typescript-eslint/no-unused-vars": "off",
+            "simple-import-sort/imports": [
+                "error",
+                {
+                    groups: [
+                        // 1. Side effect imports at the start. For me this is important because I want to import reset.css and global styles at the top of my main file.
+                        ["^\\u0000"],
+                        // 2. `react` and packages: Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+                        ["^react$", "^@?\\w"],
+                        // 3. Absolute imports and other imports such as Vue-style `@/foo`.
+                        // Anything not matched in another group. (also relative imports starting with "../")
+                        ["^@", "^"],
+                        // 4. relative imports from same folder "./" (I like to have them grouped together)
+                        ["^\\./"],
+                        // 5. style module imports always come last, this helps to avoid CSS order issues
+                        ["^.+\\.(module.css|module.scss)$"],
+                        // 6. media imports
+                        ["^.+\\.(gif|png|svg|jpg)$"],
+                    ],
+                },
+            ],
         },
     },
 ]);
