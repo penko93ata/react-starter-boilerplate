1856c8ca81362c4bd82634cd132e4034805ceac2
Penko Atanasov <atanasovpenko@gmail.com>
2025-03-30 18:21:52 +0300

    Adding prettier and eslint scripts

--- eslint.config.js
+++ eslint.config.js
@@ -29,6 +29,7 @@ export default defineConfig([
                     caughtErrorsIgnorePattern: "^_",
                 },
             ],
+            "@typescript-eslint/no-unused-vars": "off",
         },
     },
 ]);
