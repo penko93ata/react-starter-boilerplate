00939ddc13543d9654e153753646ae60b23f5425
Penko Atanasov <atanasovpenko@gmail.com>
2025-03-30 20:28:44 +0300

    Adding path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        viteTsConfigPaths({
+            projects: ["./tsconfig.json"],
+        }),
+    ],
 });
