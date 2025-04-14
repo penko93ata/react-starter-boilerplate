0846de1f5ca3838fa67819da63a34813f400953b
Penko Atanasov <atanasovpenko@gmail.com>
2025-04-14 20:18:27 +0300

    Adding tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,6 @@ function App() {
 
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank" rel="noreferrer">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
@@ -26,6 +18,7 @@ function App() {
                 </p>
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <h1 className="underline text-3xl font-bold">Hello world!</h1>
         </>
     );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
         viteTsConfigPaths({
             projects: ["./tsconfig.json"],
         }),
+        tailwindcss(),
     ],
 });
